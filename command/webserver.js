b1d2884058fc703532bbbbceca1d6040b4423cb6
mahasch <mahasch140@gmail.com>
2022-05-24 12:55:04 +0100

    working webstie base

--- command/webserver.js
+++ command/webserver.js
@@ -1,8 +1,21 @@
 var http = require('http');
+let htmlContent = `
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Page Title</title>
+ </head>
+ <body style="background: linear-gradient(to bottom, #ffff99 0%, #00ffff 100%)">
+ <h1 style="font-family:verdana;">This is a heading</h1>
+ <p style="font-family:courier;">This is a paragraph.</p>
+ </body>
+</html>
+`;
+
+
 var server = http.createServer(function(req, res){
- let htmlContent = "";
-res.writeHead(200, {'Content-Type':'text/html'})
-res.end(htmlContent);
+ res.writeHead(200, {'Content-Type':'text/html'})
+ res.end(htmlContent);
 });
 console.log('Server is running on port 3000');
-server.listen(3000,'0.0.0.0');
+server.listen(3000,'127.0.0.1');
