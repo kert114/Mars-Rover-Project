3d560bb22a272977ce292d2c8ab6b3412a76aa1c
mahasch <mahasch140@gmail.com>
2022-05-24 23:28:33 +0100

    website almost done

--- command/webserver.js
+++ command/webserver.js
@@ -1,61 +1,59 @@
 var http = require('http');
 let htmlContent = `
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
- <head>
- <title>Page Title</title>
- </head>
- <body style="background: linear-gradient(to bottom, #ffff99 0%, #00ffff 100%)">
- <h1 style="font-family:verdana;">This is a heading</h1>
- <p style="font-family:courier;">This is a paragraph.</p>
- </body>
-</html>
+<head>
+	<script type="text/javascript">
+	window.onload = function () {
 
+      var dps = [
+  
+      ];   //dataPoints. 
 
+      var chart = new CanvasJS.Chart("chartContainer",{
+        zoomEnabled: true,
+        panEnabled: true,
+        
+      	title :{
+      		text: "Mapping of Arena"
+      	},
+      	axisX: {						
+      		title: "X domain"
+      	},
+      	axisY: {						
+      		title: "Y Domain"
+      	},
+      	data: [{
+      		type: "scatter",
+      		dataPoints : dps
+      	}]
+      });
 
-<! scatter plots>
-<html>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-<body>
-<canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+      chart.render();	
+      var updateInterval = 1000;
 
-<script>
-var xyValues = [
-  {x:50, y:7},
-  {x:60, y:8},
-  {x:70, y:8},
-  {x:80, y:9},
-  {x:90, y:9},
-  {x:100, y:9},
-  {x:110, y:10},
-  {x:120, y:11},
-  {x:130, y:14},
-  {x:140, y:14},
-  {x:150, y:15}
-];
+      var updateChart = function () {
+      	
+      	xVal = Math.floor((Math.random() * 100));
+      	yVal =Math.floor((Math.random() * 100));
+      	dps.push({x: xVal,y: yVal});
+      	
+      	chart.render();		
 
-new Chart("myChart", {
-  type: "scatter",
-  data: {
-    datasets: [{
-      pointRadius: 4,
-      pointBackgroundColor: "rgb(0,0,255)",
-      data: xyValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    scales: {
-      xAxes: [{ticks: {min: 40, max:160}}],
-      yAxes: [{ticks: {min: 6, max:16}}],
-    }
-  }
-});
+	// update chart after specified time. 
+
+};
+
+setInterval(function(){updateChart()}, updateInterval); 
+}
 </script>
-
+<script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</head>
+<body>
+	<div id="chartContainer" style="height: 300px; width: 100%;">
+	</div>
 </body>
 </html>
-
 `;
 
 
