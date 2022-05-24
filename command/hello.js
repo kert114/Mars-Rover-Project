917f62e3b2ee3fb06d947762ba2662110d1732ba
mahasch <mahasch140@gmail.com>
2022-05-24 12:03:24 +0100

    initial commit

---
+++ command/hello.js
@@ -0,0 +1,15 @@
+let num = 5, username = "s.baig", domain = "@imperial.ac.uk", type = true;
+console.log(username+domain)
+
+let person = {
+    name: "Sarim Baig",
+    email: "s.baig@imperial.ac.uk",
+    numOfModules: 4
+   };
+   console.log(person);
+   console.log(person.email);
+
+   let persons = ["Sarim", "Max", "Harry"];
+   console.log(persons);
+   console.log(persons[0]);
+   
