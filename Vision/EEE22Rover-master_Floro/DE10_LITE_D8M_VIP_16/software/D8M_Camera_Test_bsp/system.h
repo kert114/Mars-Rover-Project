019e46474ce4eea51632fb9f02aeb36203bf292b
FloroBalzer <94688174+FloroBalzer@users.noreply.github.com>
2022-05-31 14:01:43 +0100

    Bounding Boxes V1
    It might work?

--- Vision/EEE22Rover-master_Floro/DE10_LITE_D8M_VIP_16/software/D8M_Camera_Test_bsp/system.h
+++ Vision/EEE22Rover-master_Floro/DE10_LITE_D8M_VIP_16/software/D8M_Camera_Test_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2' in SOPC Builder design 'Qsys'
  * SOPC Builder design path: ../../Qsys.sopcinfo
  *
- * Generated: Mon May 30 23:34:48 BST 2022
+ * Generated: Tue May 31 13:54:33 BST 2022
  */
 
 /*
@@ -465,7 +465,7 @@
 #define SYSID_QSYS_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define SYSID_QSYS_NAME "/dev/sysid_qsys"
 #define SYSID_QSYS_SPAN 8
-#define SYSID_QSYS_TIMESTAMP 1653949797
+#define SYSID_QSYS_TIMESTAMP 1654001241
 #define SYSID_QSYS_TYPE "altera_avalon_sysid_qsys"
 
 
