95e2ce91654a2db0bd0161748f721d797ef364db
FloroBalzer <94688174+FloroBalzer@users.noreply.github.com>
2022-06-03 21:57:46 +0100

    Filter Edits, and changes to bounding boxes
    shit is broken af once again, might be due to too tight value bounds? No clue, shall continue investigating

--- Vision/EEE22Rover-master_Floro/DE10_LITE_D8M_VIP_16/software/D8M_Camera_Test_bsp/system.h
+++ Vision/EEE22Rover-master_Floro/DE10_LITE_D8M_VIP_16/software/D8M_Camera_Test_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2' in SOPC Builder design 'Qsys'
  * SOPC Builder design path: ../../Qsys.sopcinfo
  *
- * Generated: Wed Jun 01 17:12:27 BST 2022
+ * Generated: Fri Jun 03 21:46:49 BST 2022
  */
 
 /*
@@ -465,7 +465,7 @@
 #define SYSID_QSYS_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define SYSID_QSYS_NAME "/dev/sysid_qsys"
 #define SYSID_QSYS_SPAN 8
-#define SYSID_QSYS_TIMESTAMP 1654099671
+#define SYSID_QSYS_TIMESTAMP 1654288931
 #define SYSID_QSYS_TYPE "altera_avalon_sysid_qsys"
 
 
