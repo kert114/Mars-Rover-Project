6377f2ef14d70483398985b1bf0367981776e19d
Angus Parry <angus.parry20@imperial.ac.uk>
2022-06-22 17:42:08 +0100

    Added object avoidance to the object avoidance file

--- obstacle_avoidance.cpp
+++ obstacle_avoidance.cpp
@@ -8,17 +8,21 @@
 float distance_to_alien = 0;
 float angle_from_vision = 0;
 float current_angle = 0;
-bool alien_detected = 0;
+bool alien_detected = false;
 int target_x = 0;
 int target_y = 0;
 float total_x_overall = 0;
 float total_y_overall = 0;
-bool facing_target = 0;
-bool dest = 0;
+bool facing_target = false;
+bool dest = false;
+bool object_noted = false;
+float initial_angle = 0;
+float width_of_rover = 10.5;
+bool interacting = false;
 
 void turn_gyro_angle(float theta){}
 void go_forwards(float y){}
-void rover_break(){}
+void brake_rover(){}
 void delay(int milliseconds){}
 
 void turn_to_object(){
@@ -33,36 +37,70 @@ void turn_to_object(){
   turn_gyro_angle(angle_to_drive_to); // this needs to return the function so that it turns. Then in the main script, call another function that doesn't do this. 
 }
 
-std::string obstacle_avoidance(){
+void object_avoid(int iteration, float temp_angle){
+  if(iteration == 1){
+    initial_angle = current_angle;
+  }
+  temp_angle += 10;
+  if((initial_angle+temp_angle)>180){
+    temp_angle-=360;
+  }
+  turn_gyro_angle(initial_angle+temp_angle);
+  if(alien_detected && object_noted){
+    if(abs(angle_from_vision)>-18 && distance_to_alien<=20){
+      iteration+=1;
+      object_avoid(iteration, temp_angle);
+    }
+  }
+  float hypotenuse = 20/tan(temp_angle*(M_PI/180));
+  go_forwards(hypotenuse);
+  turn_gyro_angle(initial_angle-temp_angle);
+  go_forwards(hypotenuse);
+  turn_gyro_angle(initial_angle);
+  // delta_y -= (2*hypotenuse-40); // this would correct the y value so the rover keeps driving to the correct point?
+}
+
+void object_interaction(){ // need to call this within the go_forwards function within an if(interacting = false);
+  interacting = true;
   float move_forward_by;
-  std::string command_to_drive;
   float at_angle;
   // continue driving to node 
-  if(!alien_detected){ // && !dest){ // don't think we need the second condition as when dest is reached new target should be used to move to
-    move_forward_by = sqrt(pow((target_x - total_x_overall),2) + pow((target_y - total_y_overall),2));
-    at_angle = 90 - atan(abs(((target_y - total_y_overall)/(target_x - total_x_overall)))); // if no alien detected, drive forward. 
-    turn_gyro_angle(at_angle);
-    delay(50);
-    go_forwards(move_forward_by);
-  }  
-  if(alien_detected){ // && !dest){ // don't think we need the second condition as when dest is reached new target should be used to move to
+  // if(!alien_detected){ // && !dest){ // don't think we need the second condition as when dest is reached new target should be used to move to
+  //   move_forward_by = sqrt(pow((target_x - total_x_overall),2) + pow((target_y - total_y_overall),2));
+  //   at_angle = 90 - atan(abs(((target_y - total_y_overall)/(target_x - total_x_overall)))); // if no alien detected, drive forward. 
+  //   turn_gyro_angle(at_angle);
+  //   delay(50);
+  //   go_forwards(move_forward_by);
+  // }  
+  if(alien_detected && !object_noted){ // && !dest){ // don't think we need the second condition as when dest is reached new target should be used to move to
     // if not at dest and alien in path 
     if(distance_to_alien<30){
-      rover_break();
+      brake_rover();
       float current_angle_saved = current_angle; // the angle at which it sees the object initially.
       delay(100);
-      turn_to_object();
       // to get in centre of LOS
+      turn_to_object();
       delay(100);
-      turn_gyro_angle(current_angle_saved);
       // return back to original angle then continue on to the original destination
+      turn_gyro_angle(current_angle_saved);
       move_forward_by = sqrt(pow((target_x - total_x_overall),2) + pow((target_y - total_y_overall),2));
       at_angle = 90 - atan(abs(((target_y - total_y_overall)/(target_x - total_x_overall)))); 
       turn_gyro_angle(at_angle);
       delay(50);
       go_forwards(move_forward_by);
+      object_noted = true;
     }
+  }else if(alien_detected && object_noted){
+    if(angle_from_vision>-18 && distance_to_alien<=20){
+      brake_rover();
+      int iteration = 1;
+      float temp_angle = 0;
+      object_avoid(iteration, temp_angle);
+    }
+  }else if(!alien_detected && object_noted){
+    object_noted = false;
   }
+  interacting = false;
 }
   // change this to recurrsion to go for more than once? 
 // or put this in the main while loop - until 6 objects are detected. 
